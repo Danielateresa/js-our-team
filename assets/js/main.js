9310cb2aee073684341f0d379f937ce63db05f7a
Danielateresa <dany.t_87@hotmail.it>
2022-10-20 13:02:30 +0200

    nitial commit  js out team

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+/*Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. 
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1: Trasformare la stringa foto in una immagine effettiva
+BONUS 2: Organizzare i singoli membri in card/schede*/
+
+//creo un array di oggetti con i dati forniti
+
+const studentsList = [
+
+    {
+        Name: 'Wayne Barnett',
+        Role: 'Founder & CEO',
+        Image: 'wayne - barnett - founder - ceo.jpg',
+    },
+    {
+        Name: 'Angela',
+        Role: 'Chief Editor',
+        Image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        Name: 'Waleter gordon',
+        Role: 'Chief Editor',
+        Image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        Name: 'Waleter gordon',
+        Role: 'Social Media Manager',
+        Image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        Name: 'Scott Estrada',
+        Role: 'Developer',
+        Image: 'scott-estrada-developer.jpg',
+    },
+    {
+        Name: 'Barbara Ramos',
+        Role: 'Grafic designer',
+        Image: 'barbara-ramos-graphic-designer.jpg',
+    }
+]
