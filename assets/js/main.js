f6db2a8a2b589bca6b9a1b4696dd48a7649e53dc
Danielateresa <dany.t_87@hotmail.it>
2022-10-20 14:04:38 +0200

    Reordered imgs

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,32 +15,32 @@ const studentsList = [
     {
         Name: 'Wayne Barnett',
         Role: 'Founder & CEO',
-        Image: `<img src="./assets/img/angela-caroll-chief-editor.jpg" alt="">`,
+        Image: `<img class ="img-fluid" src="./assets/img/wayne-barnett-founder-ceo.jpg" alt="">`,
     },
     {
-        Name: 'Angela',
+        Name: 'Angela Caroll',
         Role: 'Chief Editor',
-        Image: `<img src="./assets/img/angela-lopez-social-media-manager.jpg" alt="">`,
+        Image: `<img class ="img-fluid" src="./assets/img/angela-caroll-chief-editor.jpg" alt="">`,
     },
     {
         Name: 'Waleter gordon',
         Role: 'Chief Editor',
-        Image: `<img src="./assets/img/barbara-ramos-graphic-designer.jpg" alt="">`,
+        Image: `<img class ="img-fluid" src="./assets/img/walter-gordon-office-manager.jpg" alt="">`,
     },
     {
-        Name: 'Waleter gordon',
+        Name: 'Angela lopez',
         Role: 'Social Media Manager',
-        Image: `<img src="./assets/img/scott-estrada-developer.jpg" alt="">`,
+        Image: `<img class ="img-fluid" src="./assets/img/angela-lopez-social-media-manager.jpg" alt="">`,
     },
     {
         Name: 'Scott Estrada',
         Role: 'Developer',
-        Image: `<img src="./assets/img/walter-gordon-office-manager.jpg" alt="">`,
+        Image: `<img class ="img-fluid" src="./assets/img/scott-estrada-developer.jpg" alt="">`,
     },
     {
         Name: 'Barbara Ramos',
         Role: 'Grafic designer',
-        Image: `<img src="./assets/img/wayne-barnett-founder-ceo.jpg" alt="">`,
+        Image: `<img class ="img-fluid" src="./assets/img/barbara-ramos-graphic-designer.jpg" alt="">`,
     }
 ]
 
@@ -61,7 +61,7 @@ for (let i = 0; i < studentsList.length; i++) {
     //Stampare le stesse informazioni su DOM sottoforma di stringhe
     //creo gli elementi nella Dom
     const card =
-        `<div class="col card">
+        `<div class="col card  p-0  border-0 p-3 bg_light_gray text-center">
             <h3>
                 ${student.Name}
             </h3>
